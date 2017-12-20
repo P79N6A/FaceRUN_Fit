052ceca4e7f30f2112c4137008cbfa65d17e5e1a
平行线 <weikong555@gmail.com>
2017-12-20 17:22:29 +0800

    徒手锻炼

--- app/src/main/java/com/fly/run/activity/training/FitTimeActivity.java
+++ app/src/main/java/com/fly/run/activity/training/FitTimeActivity.java
@@ -1,9 +1,8 @@
 package com.fly.run.activity.training;
 
 import android.os.Bundle;
-import android.support.v7.widget.GridLayoutManager;
-import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.StaggeredGridLayoutManager;
 import android.view.View;
 
 import com.fly.run.R;
@@ -18,12 +17,12 @@ public class FitTimeActivity extends BaseUIActivity {
     private CommonActionBar actionBar;
     private RecyclerView mRecyclerView;
     private FitRecyclerAdapter mAdapter;
-    private RecyclerView mRecyclerViewUp;
-    private FitRecyclerAdapter mAdapterUp;
-    private RecyclerView mRecyclerViewMid;
-    private FitRecyclerAdapter mAdapterMid;
+    //    private RecyclerView mRecyclerViewUp;
+//    private FitRecyclerAdapter mAdapterUp;
+//    private RecyclerView mRecyclerViewMid;
+//    private FitRecyclerAdapter mAdapterMid;
     private int clickCount = 0;
-    private final String[] FitTrains = {"俯卧撑", "仰卧卷腹", "徒手深蹲"};
+    private String[] FitTrains = {"俯卧撑", "仰卧卷腹", "徒手深蹲"};
     private final String[] AllFitTrains = {"俯卧撑", "倒立撑", "引体向上", "仰卧起坐", "仰卧卷腹", "平板支撑", "徒手深蹲", "负重深蹲", "箭步蹲"};
 
     private String[] FitTrainsUp;
@@ -34,17 +33,15 @@ public class FitTimeActivity extends BaseUIActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fit_time);
-        FitTrainsUp = getResources().getStringArray(R.array.array_fit_up);
-        FitTrainsMid = getResources().getStringArray(R.array.array_fit_mid);
-        FitTrainsDown = getResources().getStringArray(R.array.array_fit_down);
+        FitTrains = getResources().getStringArray(R.array.array_fits);
+//        FitTrainsMid = getResources().getStringArray(R.array.array_fit_mid);
+//        FitTrainsDown = getResources().getStringArray(R.array.array_fit_down);
         initView();
         loadData();
     }
 
     private void initView() {
         initActionBar();
-        initRecyclerViewUp();
-        initRecyclerViewMid();
         initRecyclerView();
     }
 
@@ -59,58 +56,22 @@ public class FitTimeActivity extends BaseUIActivity {
         });
     }
 
-    private void initRecyclerViewUp() {
-        //得到控件
-        mRecyclerViewUp = (RecyclerView) findViewById(R.id.recyclerViewUp);
-        //设置布局管理器
-        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
-        linearLayoutManager.setOrientation(LinearLayoutManager.HORIZONTAL);
-        mRecyclerViewUp.setLayoutManager(linearLayoutManager);
-        //设置适配器
-        mAdapterUp = new FitRecyclerAdapter(this);
-        mRecyclerViewUp.setAdapter(mAdapterUp);
-    }
-
-    private void initRecyclerViewMid() {
-        //得到控件
-        mRecyclerViewMid = (RecyclerView) findViewById(R.id.recyclerViewMid);
-        //设置布局管理器
-        GridLayoutManager gridLayoutManager = new GridLayoutManager(this, 2);
-        mRecyclerViewMid.setLayoutManager(gridLayoutManager);
-        //设置适配器
-        mAdapterMid = new FitRecyclerAdapter(this);
-        mRecyclerViewMid.setAdapter(mAdapterMid);
-    }
-
     private void initRecyclerView() {
         //得到控件
         mRecyclerView = (RecyclerView) findViewById(R.id.recyclerView);
-        //设置布局管理器
-        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
-        linearLayoutManager.setOrientation(LinearLayoutManager.HORIZONTAL);
-        mRecyclerView.setLayoutManager(linearLayoutManager);
-        mRecyclerView.addOnScrollListener(new RecyclerView.OnScrollListener() {
-            @Override
-            public void onScrollStateChanged(RecyclerView recyclerView, int newState) {
-                super.onScrollStateChanged(recyclerView, newState);
-            }
-
-            @Override
-            public void onScrolled(RecyclerView recyclerView, int dx, int dy) {
-                super.onScrolled(recyclerView, dx, dy);
-            }
-        });
+//        //设置布局管理器
+//        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
+//        linearLayoutManager.setOrientation(LinearLayoutManager.HORIZONTAL);
+//        mRecyclerView.setLayoutManager(linearLayoutManager);
+        //设置layoutManager
+        mRecyclerView.setLayoutManager(new StaggeredGridLayoutManager(3, StaggeredGridLayoutManager.VERTICAL));
         //设置适配器
         mAdapter = new FitRecyclerAdapter(this);
         mRecyclerView.setAdapter(mAdapter);
     }
 
     private void loadData() {
-        mAdapterUp.setData(Arrays.asList(FitTrainsUp));
-        mAdapterUp.notifyDataSetChanged();
-        mAdapterMid.setData(Arrays.asList(FitTrainsMid));
-        mAdapterMid.notifyDataSetChanged();
-        mAdapter.setData(Arrays.asList(FitTrainsDown));
+        mAdapter.setData(Arrays.asList(FitTrains));
         mAdapter.notifyDataSetChanged();
     }
 }
--- app/src/main/java/com/fly/run/adapter/FitRecyclerAdapter.java
+++ app/src/main/java/com/fly/run/adapter/FitRecyclerAdapter.java
@@ -1,16 +1,20 @@
 package com.fly.run.adapter;
 
 import android.content.Context;
+import android.graphics.Color;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import com.fly.run.R;
+import com.fly.run.utils.DisplayUtil;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 /**
  * Created by kongwei on 2017/3/14.
@@ -21,10 +25,12 @@ public class FitRecyclerAdapter extends RecyclerView.Adapter<FitRecyclerAdapter.
     private Context mContext;
     private LayoutInflater layoutInflater;
     private List<String> datas = new ArrayList<>();
+    private int minItemHeight = 130;
 
     public FitRecyclerAdapter(Context context) {
         this.mContext = context;
         layoutInflater = LayoutInflater.from(mContext);
+        minItemHeight = DisplayUtil.dp2px(130);
     }
 
     public void setData(List<String> datas) {
@@ -45,6 +51,17 @@ public class FitRecyclerAdapter extends RecyclerView.Adapter<FitRecyclerAdapter.
     @Override
     public void onBindViewHolder(MyViewHolder holder, int position) {
         holder.tv.setText(datas.get(position));
+        Random random = new Random();
+        int result = DisplayUtil.dp2px((random.nextInt(10) + 1) * 30);
+        if (result < minItemHeight) {
+            result = minItemHeight;
+        }
+        RelativeLayout.LayoutParams params = (RelativeLayout.LayoutParams) holder.tv.getLayoutParams();
+        params.height = result;
+        int r = random.nextInt(200) + 55;
+        int g = random.nextInt(200) + 55;
+        int b = random.nextInt(200) + 55;
+        holder.tv.setBackgroundColor(Color.argb(255, r, g, b));
     }
 
     @Override
